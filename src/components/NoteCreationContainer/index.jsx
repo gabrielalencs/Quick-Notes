e10157efdad413a93aa19cd36a3485a4f8728ef2
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-09-19 19:04:47 -0300

    feat: adjustments to the notes section

--- src/components/NoteCreationContainer/index.jsx
+++ src/components/NoteCreationContainer/index.jsx
@@ -1,9 +1,22 @@
+// Reacy
+
 import { useEffect, useState, useRef } from 'react';
+
+// Tippy.js
+
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/shift-away.css';
+
+// React Icons
+
 import { FaBold, FaItalic } from "react-icons/fa";
 import { MdFormatUnderlined } from "react-icons/md";
+import { IoMicOutline } from "react-icons/io5";
+import { FaRegSquare } from "react-icons/fa";
+import { ImBin } from "react-icons/im";
+import { FiSave } from "react-icons/fi";
+
 
 const NoteCreationContainer = () => {
     const [textFormatting, setTextFormatting] = useState([]);
@@ -13,14 +26,12 @@ const NoteCreationContainer = () => {
 
         containerNoteRef.current.focus();
 
-        // Atualizar o estado da formatação
         setTextFormatting((prevFormats) => {
             return prevFormats.includes(formatType)
                 ? prevFormats.filter((type) => type !== formatType)
                 : [...prevFormats, formatType];
         });
 
-        // Aplicar a formatação ao texto selecionado
         applyFormatting(formatType);
     };
 
@@ -43,40 +54,46 @@ const NoteCreationContainer = () => {
             tippy(button, {
                 content: tooltipsContent[index],
                 animation: "shift-away",
-                theme: "textFormattingButtonTippy",
+                theme: "tippyPrimary",
             });
         });
     }, []);
 
     return (
-        <div className="border border-blue-header rounded-md max-w-[700px]">
-            <div className="py-4 px-5 flex items-center gap-5 text-white bg-blue-container-notes-header">
-                <button
-                    className={`textFormattingButton ${textFormatting.includes('font-bold') ? 'text-blue-buttons' : ''}`}
-                    onClick={() => toggleTextFormatting('font-bold')}
-                >
-                    <FaBold />
-                </button>
-                <button
-                    className={`textFormattingButton ${textFormatting.includes('italic') ? 'text-blue-buttons' : ''}`}
-                    onClick={() => toggleTextFormatting('italic')}
-                >
-                    <FaItalic />
-                </button>
-                <button
-                    className={`textFormattingButton ${textFormatting.includes('underline') ? 'text-blue-buttons' : ''}`}
-                    onClick={() => toggleTextFormatting('underline')}
-                >
-                    <MdFormatUnderlined className="text-xl relative top-[0.5px]" />
-                </button>
+
+        <div>
+            <div className="border border-blue-header rounded-md max-w-[700px]">
+                <div className="py-4 px-5 flex items-center gap-5 text-white bg-blue-container-notes-header">
+                    <button
+                        className={`textFormattingButton ${textFormatting.includes('font-bold') ? 'text-blue-buttons' : ''}`}
+                        onClick={() => toggleTextFormatting('font-bold')}
+                    >
+                        <FaBold />
+                    </button>
+                    <button
+                        className={`textFormattingButton ${textFormatting.includes('italic') ? 'text-blue-buttons' : ''}`}
+                        onClick={() => toggleTextFormatting('italic')}
+                    >
+                        <FaItalic />
+                    </button>
+                    <button
+                        className={`textFormattingButton ${textFormatting.includes('underline') ? 'text-blue-buttons' : ''}`}
+                        onClick={() => toggleTextFormatting('underline')}
+                    >
+                        <MdFormatUnderlined className="text-xl relative top-[0.5px]" />
+                    </button>
+                </div>
+
+                <div
+                    className='bg-blue-container-notes h-[350px] p-5 outline-none text-white'
+                    ref={containerNoteRef}
+                    contentEditable
+                    suppressContentEditableWarning />
             </div>
 
-            <div
-                className='bg-blue-container-notes h-[350px] p-5 outline-none text-white'
-                ref={containerNoteRef}
-                contentEditable
-                suppressContentEditableWarning
-            ></div>
+            <div>
+                
+            </div>
         </div>
     );
 };
