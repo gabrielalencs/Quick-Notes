cf7bce6f674934a4325654eab76fee117d299579
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-09-18 22:18:30 -0300

    feat: finalizes logic for formatting notepad text

--- src/components/NoteCreationContainer/index.jsx
+++ src/components/NoteCreationContainer/index.jsx
@@ -1,5 +1,3 @@
-// React
-
 import { useEffect, useState, useRef } from 'react';
 
 // Tippy.js
@@ -11,23 +9,54 @@ import 'tippy.js/dist/svg-arrow.css';
 
 // React Icons
 
-import { FaBold } from "react-icons/fa6";
+import { FaBold } from "react-icons/fa";
 import { FaItalic } from "react-icons/fa";
 import { MdFormatUnderlined } from "react-icons/md";
 
 
 const NoteCreationContainer = () => {
+    const [textFormatting, setTextFormatting] = useState([]);
 
-    const [noteText, setNoteText] = useState('');
     const containerNoteRef = useRef(null);
 
-    window.addEventListener('click', (event) => {
-        if (containerNoteRef.current && event.target.closest('.textFormattingButton')) {
-            containerNoteRef.current.focus();
-        }
-    });
+    console.log(textFormatting);
 
-    // Tippy.js
+
+    const toggleTextFormatting = (formatType) => {
+
+        let updateFormating;
+
+        if (textFormatting.includes(formatType)) {
+            updateFormating = textFormatting.filter(type => type !== formatType)
+        } else {
+            updateFormating = [...textFormatting, formatType]
+        }
+
+        setTextFormatting(updateFormating);
+    };
+
+
+    const applyFormatting = () => {
+        containerNoteRef.current.focus();
+
+        const commands = {
+            'font-bold': 'bold',
+            'italic': 'italic',
+            'underline': 'underline'
+        };
+
+        textFormatting.forEach((format) => {
+            if (commands[format]) {
+                document.execCommand(commands[format], false, null);
+            }
+        });
+    };
+
+
+    useEffect(() => {
+        applyFormatting();
+    }, [textFormatting]);
+
 
     useEffect(() => {
         const tooltipsContent = [
@@ -45,34 +74,47 @@ const NoteCreationContainer = () => {
         });
     }, []);
 
-
-
     return (
-        <div className=" border border-blue-header rounded-md max-w-[700px]">
+        <div className="border border-blue-header rounded-md max-w-[700px]">
             <div className="py-4 px-5 flex items-center gap-5 text-white bg-blue-container-notes-header">
-                <button id="boldButton">
-                    <FaBold className="textFormattingButton" />
+                <button
+                    className={`textFormattingButton ${textFormatting.includes('font-bold') ? 'text-blue-buttons' : ''}`}
+                    onClick={() => {
+                        toggleTextFormatting('font-bold');
+                        applyFormatting();
+                    }}
+                >
+                    <FaBold />
                 </button>
-
-                <button id="italicButton">
-                    <FaItalic className="textFormattingButton" />
+                <button
+                    className={`textFormattingButton ${textFormatting.includes('italic') ? 'text-blue-buttons' : ''}`}
+                    onClick={() => {
+                        toggleTextFormatting('italic');
+                        applyFormatting();
+                    }}
+                >
+                    <FaItalic />
                 </button>
-
-                <button id="underlineButton">
-                    <MdFormatUnderlined className="textFormattingButton text-xl relative top-[0.5px]" />
+                <button
+                    className={`textFormattingButton ${textFormatting.includes('underline') ? 'text-blue-buttons' : ''}`}
+                    onClick={() => {
+                        toggleTextFormatting('underline');
+                        applyFormatting();
+                    }}
+                >
+                    <MdFormatUnderlined className="text-xl relative top-[0.5px]" />
                 </button>
             </div>
 
-            <div className='bg-blue-container-notes h-[350px] p-5 outline-none'>
-                <textarea
-                    className='bg-transparent text-white inline-block h-full w-full resize-none focus:outline-none'
-                    ref={containerNoteRef}
-                    value={noteText}
-                    onChange={(event) => setNoteText(event.target.value)}
-                />
+            <div
+                className='bg-blue-container-notes h-[350px] p-5 outline-none text-white'
+                ref={containerNoteRef}
+                contentEditable
+                suppressContentEditableWarning>
+                
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default NoteCreationContainer
+export default NoteCreationContainer;
