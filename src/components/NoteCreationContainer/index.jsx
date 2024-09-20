f80ed0aacf2778580c14cba0e6807e427c794a35
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-09-20 20:14:39 -0300

    feat: starts speech consideration functionality

--- src/components/NoteCreationContainer/index.jsx
+++ src/components/NoteCreationContainer/index.jsx
@@ -1,37 +1,35 @@
-// Reacy
-
 import { useEffect, useState, useRef } from 'react';
 
 // Tippy.js
-
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/shift-away.css';
 
 // React Icons
-
 import { FaBold, FaItalic } from "react-icons/fa";
 import { MdFormatUnderlined } from "react-icons/md";
 import { IoMicOutline } from "react-icons/io5";
 import { FaRegSquare } from "react-icons/fa";
-import { ImBin } from "react-icons/im";
+import { RiDeleteBinLine } from "react-icons/ri";
 import { FiSave } from "react-icons/fi";
-
 
 const NoteCreationContainer = () => {
     const [textFormatting, setTextFormatting] = useState([]);
+    const [recognitionInstance, setRecognitionInstance] = useState(null);
+    const [transcriptText, setTranscriptText] = useState('');
+    const [interimText, setInterimText] = useState('');
+
+
+
     const containerNoteRef = useRef(null);
 
     const toggleTextFormatting = (formatType) => {
-
         containerNoteRef.current.focus();
-
         setTextFormatting((prevFormats) => {
             return prevFormats.includes(formatType)
                 ? prevFormats.filter((type) => type !== formatType)
                 : [...prevFormats, formatType];
         });
-
         applyFormatting(formatType);
     };
 
@@ -48,9 +46,89 @@ const NoteCreationContainer = () => {
         }
     };
 
+    // Iniciar gravação de fala
+    const startRecognition = () => {
+        setRecognitionInstance(true);
+
+        window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+        if (window.SpeechRecognition) {
+            const recognition = new SpeechRecognition();
+            recognition.lang = 'pt-BR';
+            recognition.continuous = true;
+            recognition.interimResults = true;
+
+            recognition.onresult = (event) => {
+                let finalTranscript = '';
+                let interimTranscript = '';
+
+                for (let i = event.resultIndex; i < event.results.length; i++) {
+                    const transcript = event.results[i][0].transcript;
+
+                    if (event.results[i].isFinal) {
+                        // Acumula o texto finalizado
+                        finalTranscript += transcript;
+                    } else {
+                        // Exibe o texto provisório
+                        interimTranscript += transcript;
+                    }
+                }
+
+                // Atualiza o texto final e provisório
+                setTranscriptText((prevText) => prevText + finalTranscript); // Acumula o texto final
+                setInterimText(interimTranscript); // Exibe o texto provisório
+            };
+
+            recognition.onerror = (event) => {
+                console.error('Erro no reconhecimento de fala:', event.error);
+            };
+
+            recognition.onend = () => {
+                console.log('Reconhecimento de fala terminado.');
+                setInterimText(''); // Limpa o texto provisório ao finalizar
+                setRecognitionInstance(null);
+            };
+
+            recognition.start();
+            setRecognitionInstance(recognition);
+        } else {
+            console.error('API de reconhecimento de fala não é suportada neste navegador.');
+        }
+    };
+
+    // Parar gravação de fala
+    const stopRecognition = () => {
+        if (recognitionInstance) {
+            recognitionInstance.stop();
+            setRecognitionInstance(null);
+        }
+    };
+
+
+    // useEffect(() => {
+    //     const handleInput = () => {
+    //         const textContent = containerNoteRef.current.textContent;
+    //         if (!textContent) {
+    //             // Se o conteúdo foi apagado, também apaga o texto no estado
+    //             setTranscriptText('');
+    //         }
+    //     };
+
+    //     const containerNote = containerNoteRef.current;
+    //     containerNote.addEventListener('input', handleInput);
+
+    //     return () => {
+    //         containerNote.removeEventListener('input', handleInput);
+    //     };
+    // }, []);
+
     useEffect(() => {
-        const tooltipsContent = ["Negrito", "Itálico", "Sublinhado"];
-        document.querySelectorAll('.textFormattingButton').forEach((button, index) => {
+        const tooltipsContent = [
+            "Negrito", "Itálico", "Sublinhado",
+            "Gravar voz", "Para gravação", "Limpar", "Salvar"
+        ];
+
+        document.querySelectorAll('.buttonTooltip').forEach((button, index) => {
             tippy(button, {
                 content: tooltipsContent[index],
                 animation: "shift-away",
@@ -60,24 +138,23 @@ const NoteCreationContainer = () => {
     }, []);
 
     return (
-
-        <div>
-            <div className="border border-blue-header rounded-md max-w-[700px]">
+        <div className='max-w-[700px]'>
+            <div className="border border-blue-header rounded-md">
                 <div className="py-4 px-5 flex items-center gap-5 text-white bg-blue-container-notes-header">
                     <button
-                        className={`textFormattingButton ${textFormatting.includes('font-bold') ? 'text-blue-buttons' : ''}`}
+                        className={`textFormattingButton buttonTooltip ${textFormatting.includes('font-bold') ? 'text-blue-buttons' : ''}`}
                         onClick={() => toggleTextFormatting('font-bold')}
                     >
                         <FaBold />
                     </button>
                     <button
-                        className={`textFormattingButton ${textFormatting.includes('italic') ? 'text-blue-buttons' : ''}`}
+                        className={`textFormattingButton buttonTooltip ${textFormatting.includes('italic') ? 'text-blue-buttons' : ''}`}
                         onClick={() => toggleTextFormatting('italic')}
                     >
                         <FaItalic />
                     </button>
                     <button
-                        className={`textFormattingButton ${textFormatting.includes('underline') ? 'text-blue-buttons' : ''}`}
+                        className={`textFormattingButton buttonTooltip ${textFormatting.includes('underline') ? 'text-blue-buttons' : ''}`}
                         onClick={() => toggleTextFormatting('underline')}
                     >
                         <MdFormatUnderlined className="text-xl relative top-[0.5px]" />
@@ -88,11 +165,29 @@ const NoteCreationContainer = () => {
                     className='bg-blue-container-notes h-[350px] p-5 outline-none text-white'
                     ref={containerNoteRef}
                     contentEditable
-                    suppressContentEditableWarning />
+                    suppressContentEditableWarning
+                >
+                    {transcriptText}
+                    <span style={{ opacity: 0.5 }}>{interimText}</span>
+                </div>
             </div>
 
-            <div>
-                
+            <div className='flex justify-center gap-5 mt-8'>
+                <button className='buttonsContainerNoteCreation buttonTooltip' onClick={startRecognition}>
+                    <IoMicOutline />
+                </button>
+
+                <button className='buttonsContainerNoteCreation buttonTooltip' onClick={stopRecognition}>
+                    <FaRegSquare />
+                </button>
+
+                <button className='buttonsContainerNoteCreation buttonTooltip'>
+                    <RiDeleteBinLine />
+                </button>
+
+                <button className='buttonsContainerNoteCreation buttonTooltip'>
+                    <FiSave />
+                </button>
             </div>
         </div>
     );
