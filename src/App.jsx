2b65ee37b858ff5fcfd3386fb7fafc2cd7815c6b
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-10-12 16:41:01 -0300

    feat: configuration component

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,19 @@ import NoteCreationContainer from './components/NoteCreationContainer';
 import SettingsContainer from './components/SettingsContainer';
 
 
+import React, { useState } from 'react';
+import { ChromePicker } from 'react-color';
+
+
+
 const App = () => {
+
+    const [color, setColor] = useState('#fff');
+    const [showPicker, setShowPicker] = useState(false);
+
+    const handleChangeComplete = (color) => {
+        setColor(color.hex);
+    };
 
     return (
         <>
@@ -22,6 +34,35 @@ const App = () => {
                 <div className='mt-8 flex flex-col gap-20 md:flex-row md:gap-10'>
                     <NoteCreationContainer />
                     <NoteContainer />
+                </div>
+
+
+
+                <div style={{ padding: '20px' }}>
+                    <button
+                        style={{
+                            backgroundColor: color,
+                            border: 'none',
+                            padding: '10px',
+                            borderRadius: '5px',
+                            cursor: 'pointer',
+                        }}
+                        onClick={() => setShowPicker(!showPicker)}
+                    >
+                        Selecione a Cor
+                    </button>
+
+                    {showPicker && (
+                        <div style={{ position: 'absolute', zIndex: '2' }}>
+                            <ChromePicker
+                                className="custom-chrome-picker" 
+                                color={color}
+                                onChangeComplete={handleChangeComplete}
+                            />
+                        </div>
+                    )}
+
+                    <p>Cor selecionada: {color}</p>
                 </div>
             </main>
         </>
